52c3c43ee0ea65341ba9b4bd73adc2638c026fd6
Tairo Moriyama <tairosan@gmail.com>
2013-01-09 18:53:04 +0900

    1st

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NeocareerTest::Application.config.secret_token = '249c45ed79fa38fae9d35ff74bba64e1b7280c3c77c3bba7f0343aa3d67a2f2e93d786f4f4e357d364f1e2e7b318f88924e122df7b4e678eb063e2ae06adae20'
